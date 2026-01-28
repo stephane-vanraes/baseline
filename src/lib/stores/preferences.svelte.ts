7e5af4c6be9e63b355b37c15a58790272c68562b
stephane-vanraes <stephane.vanraes@gmail.com>
2026-01-28 13:09:43 +0100

    add darkmode css

--- src/lib/stores/preferences.svelte.ts
+++ src/lib/stores/preferences.svelte.ts
@@ -1,5 +1,8 @@
+import { MediaQuery } from 'svelte/reactivity';
 import { localStorage } from './localStorage.svelte';
 
-const darkMode = localStorage('baseline:darkmode');
+const prefersDark = new MediaQuery('prefers-color-scheme: dark').current;
+
+const darkMode = localStorage('baseline:darkmode', prefersDark);
 
 export default { darkMode };
